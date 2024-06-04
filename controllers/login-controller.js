51b32234752c8a0592904d41fa92138561c85126
flutterde <chouatiotman2000@gmail.com>
2024-06-04 01:20:43 +0100

    Done

--- controllers/login-controller.js
+++ controllers/login-controller.js
@@ -1,19 +1,76 @@
+import bcrypt from 'bcryptjs';
+import dbConnect from '../utils/db-connector.js';
+import { SignJWT } from 'jose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+function validateData(body) {
+    const email = body.email;
+    const password = body.password;
+    try {
+        if (email === undefined || email === "" || email.length < 3 || email.indexOf("@") === -1 || email.indexOf(".") === -1) {
+            return ({ is_valid: false, msg: "Email is required" });
+        }
+        if (password === undefined || password === "" || password.length < 6) {
+            return ({ is_valid: false, msg: "Password is required" });
+        }
+        return ({ is_valid: true, msg: "Data is valid" });
+    } catch (error) {
+        console.log(error);
+        return ({ is_valid: false, msg: "Error validating data" });
+    }
+}
+
+async function loginUser(email, password) {
+    let client;
+    try {
+        client = await dbConnect.connect();
+        const query = `SELECT * FROM users WHERE email = $1`;
+        const result = await client.query(query, [email]);
+        client.release();
+        if (result.rows.length === 0) {
+            return ({ is_success: false, msg: "User not found" });
+        }
+        const user = result.rows[0];
+        const match = await bcrypt.compare(password, user.password);
+        if (!match) {
+            return ({ is_success: false, msg: "Invalid Credentials" });
+        }
+        user.password = undefined;
+        return ({ is_success: true, user, msg: "User Found"});
+    }
+    catch (error) {
+        console.log(error);
+        if (client) client.release();
+        return ({ is_success: false, msg: "Error logging in" });
+    }
+}
 
 
-async function loginController(req, res)
-{
-    const email = req.body.email;
-    const password = req.body.password;
-    res.setHeader('Content-Type', 'application/json');
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.status(201);
-    // wait for 5 second
-    await new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve();
-        }, 3000);
-    });
-    return res.json({ email, password });
+
+async function loginController(req, res) {
+    try {
+        const { is_valid, msg } = validateData(req.body);
+        if (!is_valid) {
+            return res.status(400).json({ msg });
+        }
+        const email = req.body.email;
+        const password = req.body.password;
+        const { is_success, user, msg: loginMsg } = await loginUser(email, password);
+        if (!is_success) {
+            return res.status(400).json({ msg: loginMsg });
+        }
+        const secret = new TextEncoder().encode(process.env.JWT_SECRET);
+        const token = await new SignJWT({ id: user.id, email: user.email })
+            .setProtectedHeader({ alg: 'HS256' })
+            .setIssuedAt()
+            .setExpirationTime('2h')
+            .sign(secret);
+        return res.status(200).json({ user, token });
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({ msg: "Error logging in" });
+    }
 }
 
 export default loginController;
--- middlewares/auth-middleware.js
+++ middlewares/auth-middleware.js
@@ -1,8 +1,35 @@
+import { jwtVerify } from 'jose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+
+async function verifyToken(token) {
+    try {
+        const splitedToken = token.split(" ")[1];
+        const secret = new TextEncoder().encode(process.env.JWT_SECRET);
+        const result = await jwtVerify(splitedToken, secret);
+        return { is_valid: true, msg: "Token is valid", user: result.payload };
+    } catch (error) {
+        console.log("Error verifying token:: ", error);
+        if (error.code === 'ERR_JWS_SIGNATURE_VERIFICATION_FAILED') {
+            console.log('Signature verification failed. Please check the token and the secret.');
+        } else if (error.code === 'ERR_JWS_INVALID') {
+            console.log('The JWT token is invalid. Please check the token and the secret.');
+        }
+        return { is_valid: false, msg: "Token is invalid" };
+    }
+}
+
 
 async function authMiddleware(req, res, next)
 {
     const token = req.headers.authorization;
     if (!token) {
+        return res.status(401).json({error: 'Unauthorized'});
+    }
+    const { is_valid, msg, user } = await verifyToken(token);
+    if (!is_valid) {
         return res.status(401).json({error: 'Unauthorized'});
     }
     next();
